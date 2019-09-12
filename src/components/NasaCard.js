fee8b2fa5c9d6da3060f2594a88d00ae99ca8845
Mary <mary.clayton1@hotmail.com>
2019-09-11 18:04:47 -0700

    save 2

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-const NasaCard = (props) => {
-
+const NasaCard = props => {
+const {key, title, date, description, url} = props
+console.log(props);
   return (
-    <div className="nasaCards">
-    <h2>{props.title}</h2>
-    <h3>{props.date}</h3>
+    <div className="nasaCards" key={key}>
+    <h2>{title}</h2>
+    <h3>Date:{date}</h3>
       <img className="nasa-image"
-        alt="NASA pic of the day" src={props.hdurl}/>
-      <p className="description">{props.explanation}</p>
+        alt="NASA pic of the day" src={url}/>
+      <p className="description">{description}</p>
     </div>
   );
 };
--- src/components/NasaImages.js
+++ src/components/NasaImages.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NasaCard from "./NasaCard";
+import axios from "axios";
+
 
 function NasaImages() {
+    const [nasa, setNasa] = useState([]);
+    //useEffect
+    useEffect(() => {
+      //axios
+      axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=g2jo6na4Gfe8IcnK5eOv2yNpqYfwmNQg2pQp9BoA&date=2012-03-14`)
+        .then(response => {
+        setNasa(nasa)
+        console.log("nasa photos",response.data);
+        })
+        .catch(error => {
+          console.log("Cant find what you're looking for", error);
+        });
+    }, [nasa]);
+  
 return (
-    <div className = "NasaImages">
-       <NasaCard />
+    <div className = "nasaImages">
+            <NasaCard 
+            key= {nasa.key}
+            title= {nasa.title} 
+            url= {nasa.hdurl} 
+            date= {nasa.date} 
+            description= {nasa.explanation} /> 
     </div>
     
     )  
